05a136872061dee6058c381ad11d2792d399d3b3
Joe J Liu <ntnoms@hotmail.com>
2019-01-07 18:58:23 +0800

    提交查库相关代码及云函数

--- miniprogram/pages/index/index.js
+++ miniprogram/pages/index/index.js
@@ -3,11 +3,7 @@ const app = getApp()
 
 Page({
   data: {
-    avatarUrl: './user-unlogin.png',
-    userInfo: {},
-    logged: false,
-    takeSession: false,
-    requestResult: ''
+
   },
 
   onLoad: function() {
@@ -34,29 +30,14 @@ Page({
         }
       }
     })
-  },
-
-  onGetUserInfo: function(e) {
-    if (!this.logged && e.detail.userInfo) {
-      this.setData({
-        logged: true,
-        avatarUrl: e.detail.userInfo.avatarUrl,
-        userInfo: e.detail.userInfo
-      })
-    }
-  },
-
-  onGetOpenid: function() {
-    // 调用云函数
+  
+    // 调用云函数获取openid
     wx.cloud.callFunction({
       name: 'login',
       data: {},
       success: res => {
         console.log('[云函数] [login] user openid: ', res.result.openid)
         app.globalData.openid = res.result.openid
-        wx.navigateTo({
-          url: '../userConsole/userConsole',
-        })
       },
       fail: err => {
         console.error('[云函数] [login] 调用失败', err)
@@ -67,54 +48,123 @@ Page({
     })
   },
 
-  // 上传图片
-  doUpload: function () {
-    // 选择图片
-    wx.chooseImage({
-      count: 1,
-      sizeType: ['compressed'],
-      sourceType: ['album', 'camera'],
-      success: function (res) {
+  // onGetUserInfo: function(e) {
+  //   if (!this.logged && e.detail.userInfo) {
+  //     this.setData({
+  //       logged: true,
+  //       avatarUrl: e.detail.userInfo.avatarUrl,
+  //       userInfo: e.detail.userInfo
+  //     })
+  //   }
+  // },
 
-        wx.showLoading({
-          title: '上传中',
+  // onGetOpenid: function() {
+  //   // 调用云函数
+  //   wx.cloud.callFunction({
+  //     name: 'login',
+  //     data: {},
+  //     success: res => {
+  //       console.log('[云函数] [login] user openid: ', res.result.openid)
+  //       app.globalData.openid = res.result.openid
+  //       wx.navigateTo({
+  //         url: '../userConsole/userConsole',
+  //       })
+  //     },
+  //     fail: err => {
+  //       console.error('[云函数] [login] 调用失败', err)
+  //       wx.navigateTo({
+  //         url: '../deployFunctions/deployFunctions',
+  //       })
+  //     }
+  //   })
+  // },
+
+  //本地版get all devices
+  // allDevices: function () {
+  //   const db = wx.cloud.database({
+  //    env: 'test-f05377'
+  //   })
+  //   db.collection('devices').get({
+  //     success: res => {
+  //       this.setData({
+  //         alldevices: res.data
+  //       })
+  //       console.log('[数据库] [查询记录] 成功: ', res)
+  //     },
+  //     fail: err => {
+  //       wx.showToast({
+  //         icon: 'none',
+  //         title: '查询记录失败'
+  //       })
+  //       console.error('[数据库] [查询记录] 失败：', err)
+  //     }
+  //   })
+  // },
+    myDevice: function () {
+      var nthis = this
+    // 本地函数，已经改用云函数
+    // const db = wx.cloud.database({
+    //   env: 'test-f05377'
+    // })
+    // db.collection('devices').where({
+    //   holding_open_id: app.globalData.openid
+    // }).field({
+    //   _id:true,name:true,os:true
+    // }).get({
+      wx.cloud.callFunction({
+        // 云函数名称
+        name: 'getmyDevices',
+        // 传给云函数的参数
+        data: {
+        },
+      success: res => {
+        console.log(res)
+        nthis.setData({
+          mydevices: res.result.data,
         })
-
-        const filePath = res.tempFilePaths[0]
-        
-        // 上传图片
-        const cloudPath = 'my-image' + filePath.match(/\.[^.]+?$/)[0]
-        wx.cloud.uploadFile({
-          cloudPath,
-          filePath,
-          success: res => {
-            console.log('[上传文件] 成功：', res)
-
-            app.globalData.fileID = res.fileID
-            app.globalData.cloudPath = cloudPath
-            app.globalData.imagePath = filePath
-            
-            wx.navigateTo({
-              url: '../storageConsole/storageConsole'
-            })
-          },
-          fail: e => {
-            console.error('[上传文件] 失败：', e)
-            wx.showToast({
-              icon: 'none',
-              title: '上传失败',
-            })
-          },
-          complete: () => {
-            wx.hideLoading()
-          }
-        })
-
+        console.log('[数据库] [查询记录] 成功: ', res)
       },
-      fail: e => {
-        console.error(e)
+      fail: err => {
+        wx.showToast({
+          icon: 'none',
+          title: '查询记录失败'
+        })
+        console.error('[数据库] [查询记录] 失败：', err)
       }
     })
   },
 
+  allDevices: function () {
+    var nthis = this
+    // 本地函数，已经改用云函数
+    // const db = wx.cloud.database({
+    //   env: 'test-f05377'
+    // })
+    // db.collection('devices').where({
+    //   holding_open_id: app.globalData.openid
+    // }).field({
+    //   _id:true,name:true,os:true
+    // }).get({
+    wx.cloud.callFunction({
+      // 云函数名称
+      name: 'getAllDevices',
+      // 传给云函数的参数
+      data: {
+      },
+      success: res => {
+        console.log(res)
+        nthis.setData({
+          mydevices: res.result.data,
+        })
+        console.log('[数据库] [查询记录] 成功: ', res)
+      },
+      fail: err => {
+        wx.showToast({
+          icon: 'none',
+          title: '查询记录失败'
+        })
+        console.error('[数据库] [查询记录] 失败：', err)
+      }
+    })
+  },
 })
