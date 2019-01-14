ec797cf0aa1b75eb3d48d1895d09eb2d59e4fff5
Joe J Liu <ntnoms@hotmail.com>
2019-01-14 19:03:49 +0800

    云函数调整，Landing page调整

--- cloudfunctions/getmyDevices/index.js
+++ cloudfunctions/getmyDevices/index.js
@@ -1,26 +1,49 @@
 // 云函数入口文件
 const cloud = require('wx-server-sdk')
 cloud.init()
-exports.main = (event, context) => {
-  console.log(event)
-  console.log(context)
-
-  // 可执行其他自定义逻辑
-  // console.log 的内容可以在云开发云函数调用日志查看
-
-  // 获取 WX Context (微信调用上下文)，包括 OPENID、APPID、及 UNIONID（需满足 UNIONID 获取条件）
-  const wxContext = cloud.getWXContext()
-  return {
-    event,
-    openid: wxContext.OPENID,
-    appid: wxContext.APPID,
-    unionid: wxContext.UNIONID,
-  }
+exports.main = async (event, context) => {
+ try {
+//     //Get userdata
+//     const wxContext = cloud.getWXContext().openid
+//     let useropenid = await wxContext
+//     //get mydevices by userdata
+   const db = cloud.database()
+  let mydevices = await db.collection('devices').where({
+    holding_open_id: event.userInfo.openId
+     }).get();
+    return mydevices
+   } 
+   catch (e) {
+     console.log(e)
+   }
 }
-const db = cloud.database()
-exports.main = async (event, context) => db.collection('devices').where({
-  holding_open_id: this.openid
-}).get()
+
+// }
+// exports.main = (event, context) => {
+//   console.log(event)
+//   console.log(context)
+
+//   // 可执行其他自定义逻辑
+//   // console.log 的内容可以在云开发云函数调用日志查看
+
+//   // 获取 WX Context (微信调用上下文)，包括 OPENID、APPID、及 UNIONID（需满足 UNIONID 获取条件）
+//   const wxContext = cloud.getWXContext()
+//   return {
+//     event,
+//     openid: wxContext.OPENID,
+//     appid: wxContext.APPID,
+//     unionid: wxContext.UNIONID,
+//   }
+
+//   console.log(openid)
+// }
+// const db = cloud.database()
+
+// //const wxContext = cloud.getWXContext().openid
+// //let useropenid = await wxContext
+//  exports.main = async (event, context) => db.collection('devices').where({
+//    holding_open_id: event.userInfo.openId
+//  }).get()
 // const db = cloud.database({
 //   env: 'test-f05377'
 // })
--- miniprogram/pages/index/index.js
+++ miniprogram/pages/index/index.js
@@ -7,50 +7,11 @@ Page({
   },
 
   onLoad: function() {
-    // 获取用户信息
-    wx.getSetting({
-      success: res => {
-        if (res.authSetting['scope.userInfo']) {
-          // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
-          wx.getUserInfo({
-            success: res => {
-              this.setData({
-                avatarUrl: res.userInfo.avatarUrl,
-                userInfo: res.userInfo
-              })
-            }
-          })
-        }
-      }
-    })
-  
-    // 调用云函数获取openid
-    wx.cloud.callFunction({
-      name: 'login',
-      data: {},
-      success: res => {
-        console.log('[云函数] [login] user openid: ', res.result.openid)
-        app.globalData.openid = res.result.openid
-      },
-      fail: err => {
-        console.error('[云函数] [login] 调用失败', err)
-        wx.navigateTo({
-          url: '../deployFunctions/deployFunctions',
-        })
-      }
-    })
+
     var nthis = this
-    // 本地函数，已经改用云函数
-    // const db = wx.cloud.database({
-    //   env: 'test-f05377'
-    // })
-    // db.collection('devices').where({
-    //   holding_open_id: app.globalData.openid
-    // }).field({
-    //   _id:true,name:true,os:true
-    // }).get({
+
     wx.cloud.callFunction({
-      // 云函数名称
+      // 获取我的设备
       name: 'getmyDevices',
       // 传给云函数的参数
       data: {
@@ -60,28 +21,19 @@ Page({
         nthis.setData({
           mydevices: res.result.data,
         })
-        console.log('[数据库] [查询记录] 成功: ', res)
+        console.log('[数据库] [查询mydevices] 成功: ', res)
       },
       fail: err => {
         wx.showToast({
           icon: 'none',
-          title: '查询记录失败'
+          title: '查询mydevices失败'
         })
-        console.error('[数据库] [查询记录] 失败：', err)
+        console.error('[数据库] [查询mydevices] 失败：', err)
       }
     })
-    var nthis = this
-    // 本地函数，已经改用云函数
-    // const db = wx.cloud.database({
-    //   env: 'test-f05377'
-    // })
-    // db.collection('devices').where({
-    //   holding_open_id: app.globalData.openid
-    // }).field({
-    //   _id:true,name:true,os:true
-    // }).get({
+
     wx.cloud.callFunction({
-      // 云函数名称
+      // 获取全部设备
       name: 'getAllDevices',
       // 传给云函数的参数
       data: {
@@ -91,135 +43,16 @@ Page({
         nthis.setData({
           alldevices: res.result.data,
         })
-        console.log('[数据库] [查询记录] 成功: ', res)
+        console.log('[数据库] [查询alldevices] 成功: ', res)
       },
       fail: err => {
         wx.showToast({
           icon: 'none',
-          title: '查询记录失败'
+          title: '查询alldevices失败'
         })
-        console.error('[数据库] [查询记录] 失败：', err)
+        console.error('[数据库] [查询alldevices] 失败：', err)
       }
     })
   },
-
-  // onGetUserInfo: function(e) {
-  //   if (!this.logged && e.detail.userInfo) {
-  //     this.setData({
-  //       logged: true,
-  //       avatarUrl: e.detail.userInfo.avatarUrl,
-  //       userInfo: e.detail.userInfo
-  //     })
-  //   }
-  // },
-
-  // onGetOpenid: function() {
-  //   // 调用云函数
-  //   wx.cloud.callFunction({
-  //     name: 'login',
-  //     data: {},
-  //     success: res => {
-  //       console.log('[云函数] [login] user openid: ', res.result.openid)
-  //       app.globalData.openid = res.result.openid
-  //       wx.navigateTo({
-  //         url: '../userConsole/userConsole',
-  //       })
-  //     },
-  //     fail: err => {
-  //       console.error('[云函数] [login] 调用失败', err)
-  //       wx.navigateTo({
-  //         url: '../deployFunctions/deployFunctions',
-  //       })
-  //     }
-  //   })
-  // },
-
-  //本地版get all devices
-  // allDevices: function () {
-  //   const db = wx.cloud.database({
-  //    env: 'test-f05377'
-  //   })
-  //   db.collection('devices').get({
-  //     success: res => {
-  //       this.setData({
-  //         alldevices: res.data
-  //       })
-  //       console.log('[数据库] [查询记录] 成功: ', res)
-  //     },
-  //     fail: err => {
-  //       wx.showToast({
-  //         icon: 'none',
-  //         title: '查询记录失败'
-  //       })
-  //       console.error('[数据库] [查询记录] 失败：', err)
-  //     }
-  //   })
-  // },
-    // myDevice: function () {
-    //   var nthis = this
-    // 本地函数，已经改用云函数
-    // const db = wx.cloud.database({
-    //   env: 'test-f05377'
-    // })
-    // db.collection('devices').where({
-    //   holding_open_id: app.globalData.openid
-    // }).field({
-    //   _id:true,name:true,os:true
-    // }).get({
-  //     wx.cloud.callFunction({
-  //       // 云函数名称
-  //       name: 'getmyDevices',
-  //       // 传给云函数的参数
-  //       data: {
-  //       },
-  //     success: res => {
-  //       console.log(res)
-  //       nthis.setData({
-  //         mydevices: res.result.data,
-  //       })
-  //       console.log('[数据库] [查询记录] 成功: ', res)
-  //     },
-  //     fail: err => {
-  //       wx.showToast({
-  //         icon: 'none',
-  //         title: '查询记录失败'
-  //       })
-  //       console.error('[数据库] [查询记录] 失败：', err)
-  //     }
-  //   })
-  // },
-
-  // allDevices: function () {
-  //   var nthis = this
-  //   // 本地函数，已经改用云函数
-  //   // const db = wx.cloud.database({
-  //   //   env: 'test-f05377'
-  //   // })
-  //   // db.collection('devices').where({
-  //   //   holding_open_id: app.globalData.openid
-  //   // }).field({
-  //   //   _id:true,name:true,os:true
-  //   // }).get({
-  //   wx.cloud.callFunction({
-  //     // 云函数名称
-  //     name: 'getAllDevices',
-  //     // 传给云函数的参数
-  //     data: {
-  //     },
-  //     success: res => {
-  //       console.log(res)
-  //       nthis.setData({
-  //         mydevices: res.result.data,
-  //       })
-  //       console.log('[数据库] [查询记录] 成功: ', res)
-  //     },
-  //     fail: err => {
-  //       wx.showToast({
-  //         icon: 'none',
-  //         title: '查询记录失败'
-  //       })
-  //       console.error('[数据库] [查询记录] 失败：', err)
-  //     }
-  //   })
-  // },
+  
 })
